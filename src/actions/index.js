c28b71363f06d7c7bbdfabd3fc248417c93451b9
L3n1ad <danieltoth0910@gmail.com>
2020-06-12 12:23:17 +0100

    initial setup for a react/redux application

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+// place Action creators here
+
+// example of an action creator with named export
+// export const selectSong = (song) => {
+//   return {
+//     type: "SONG_SELECTED",
+//     payload: song,
+//   };
+// };
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+// Replace Me is just to get rid off an initial error message, it is a dummy reducer and it's needs to be replaced
+export default combineReducers({
+  replaceMe: () => "hi there",
+});
