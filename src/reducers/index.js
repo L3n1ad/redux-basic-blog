4fd04cb24317ab27a7595a88f0c4a12f6eaf9028
L3n1ad <danieltoth0910@gmail.com>
2020-06-12 18:17:11 +0100

    fix problem not geting the right data (.data was missing from response)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 // Replace Me is just to get rid off an initial error message, it is a dummy reducer and it's needs to be replaced
 export default combineReducers({
   posts: postsReducer,
+  users: usersReducer,
 });
