a70d84b79554526e5e296d21e42e61a929c1e07c
L3n1ad <danieltoth0910@gmail.com>
2020-06-12 17:05:39 +0100

    import postsReducer to cimbineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 // Replace Me is just to get rid off an initial error message, it is a dummy reducer and it's needs to be replaced
 export default combineReducers({
-  replaceMe: () => "hi there",
+  posts: postsReducer,
 });
