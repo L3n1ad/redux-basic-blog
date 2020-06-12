eeca0d07a861a5b9b67e492ed3fce8b8659f2754
L3n1ad <danieltoth0910@gmail.com>
2020-06-12 17:06:19 +0100

    pass posts as props to PostList component from redux

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,12 +2,19 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
-const PostList = ({ fetchPosts }) => {
+const PostList = ({ fetchPosts, posts }) => {
   useEffect(() => {
     fetchPosts();
-  });
+  }, []);
+
+  console.log(posts);
+
   return <div>I am POST LIST</div>;
 };
 
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
 // even if we don't have a state we need pass in something as a first argument for connect
-export default connect(null, { fetchPosts: fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostList);
